59411088fa432ac87b5042b27baecada834029d2
Julian Torres <julianftorres320@gmail.com>
2023-09-26 12:08:22 -0500

    Complemento de la actividad

--- Equipo.java
+++ Equipo.java
@@ -1,40 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Equipo {
     private String nombreEquipo;
     private String paisEquipo;
     private static double sumaTiempos;
+    private List<Ciclista> ciclistas; // Lista para almacenar ciclistas
+
     public Equipo(String nombreEquipo, String paisEquipo) {
         this.nombreEquipo = nombreEquipo;
         this.paisEquipo = paisEquipo;
+        this.ciclistas = new ArrayList<>(); // Inicializa la lista de ciclistas
     }
+
     public String getNombreEquipo() {
         return nombreEquipo;
     }
+
     public void setNombreEquipo(String nombreEquipo) {
         this.nombreEquipo = nombreEquipo;
     }
+
     public String getPaisEquipo() {
         return paisEquipo;
     }
+
     public void setPaisEquipo(String paisEquipo) {
         this.paisEquipo = paisEquipo;
     }
+
     public static double getSumaTiempos() {
         return sumaTiempos;
     }
+
     public static void setSumaTiempos(double sumaTiempos) {
         Equipo.sumaTiempos = sumaTiempos;
     }
-    protected void añadirCiclista(){
-        //agregar cada uno de los ciclistas
-    }
-    protected void listarEquipo(){
-        //Nombre del equipo
-        //País del equipo
-        //Integrantes del equipo
+
+    // Método para añadir un ciclista al equipo
+    public void añadirCiclista(Ciclista ciclista) {
+        ciclistas.add(ciclista);
     }
 
-    
+    // Método para listar el equipo y sus ciclistas
+    public void listarEquipo() {
+        System.out.println("Nombre del equipo: " + nombreEquipo);
+        System.out.println("País del equipo: " + paisEquipo);
+        System.out.println("Integrantes del equipo:");
 
-    
-
+        for (Ciclista ciclista : ciclistas) {
+            System.out.println(" - Nombre: " + ciclista.getNombre());
+            System.out.println("   Tipo: " + ciclista.imprimirTipo());
+            ciclista.imprimir(); // Imprime detalles específicos del ciclista
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -5,7 +5,18 @@ public class Main {
         Escalador escalador = new Escalador(5678, "Nairo Quintana", 20, 15);
         Contrarelojista contrarelojista1 = new Contrarelojista(1357, "Esteban Chavez", 120);
         //Imprimir listar equipo
+        
+
+        // Agregar los ciclistas al equipo usando el método añadirCiclista
+        equipo1.añadirCiclista(velocista1);
+        equipo1.añadirCiclista(escalador);
+        equipo1.añadirCiclista(contrarelojista1);
+
+        // Listar el equipo y sus ciclistas usando el método listarEquipo
+        equipo1.listarEquipo();
+    }
+
     
     }
 
-}
+
